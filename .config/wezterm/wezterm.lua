a82b4907bace24000f555aa06d503778f21f6bbd
wancup <62416191+wancup@users.noreply.github.com>
2023-03-18 22:06:45 +0900

    Format

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,14 +1,14 @@
 local wezterm = require "wezterm"
 
 return {
-    font = wezterm.font_with_fallback {
-        { family = 'UDEV Gothic 35JPDOC', assume_emoji_presentation = false },
-        { family = "Noto Emoji",        weight = 700 },
-    },
-    use_ime = true,
-    font_size = 14,
-    unicode_version = 14,
-    initial_cols = 160,
-    initial_rows = 48,
-    color_scheme = "Duotone Dark",
+  font = wezterm.font_with_fallback {
+    { family = "UDEV Gothic 35JPDOC", assume_emoji_presentation = false },
+    { family = "Noto Emoji",          weight = 700 },
+  },
+  font_size = 14,
+  use_ime = true,
+  unicode_version = 14,
+  initial_cols = 160,
+  initial_rows = 48,
+  color_scheme = "Duotone Dark",
 }
