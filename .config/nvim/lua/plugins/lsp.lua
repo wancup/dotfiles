ebcaca38a3c2d76c482f2748191829c03e1fc3b8
wancup <62416191+wancup@users.noreply.github.com>
2023-03-30 23:31:51 +0900

    Avoid default lsp formatting

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -101,6 +101,15 @@ return {
       },
     },
     opts = function()
+      local lsp_formatting = function(bufnr)
+        vim.lsp.buf.format({
+          filter = function(client)
+            return client.name == "null-ls"
+          end,
+          bufnr = bufnr,
+        })
+      end
+
       local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
       local on_attach = function(client, bufnr)
         if client.supports_method("textDocument/formatting") then
@@ -109,7 +118,7 @@ return {
             group = augroup,
             buffer = bufnr,
             callback = function()
-              vim.lsp.buf.format({ bufnr = bufnr })
+              lsp_formatting(bufnr)
             end,
           })
         end
