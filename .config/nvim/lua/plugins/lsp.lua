18119e67c55810e54b909e0fe35611a8cc6e2252
wancup <62416191+wancup@users.noreply.github.com>
2023-04-18 21:20:36 +0900

    Change to use eslint_d
    * https://github.com/jose-elias-alvarez/null-ls.nvim/issues/596

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -130,8 +130,8 @@ return {
         sources = {
           null_ls.builtins.formatting.shfmt,
           null_ls.builtins.code_actions.gitsigns,
-          null_ls.builtins.diagnostics.eslint,
-          null_ls.builtins.code_actions.eslint,
+          null_ls.builtins.diagnostics.eslint_d,
+          null_ls.builtins.code_actions.eslint_d,
           null_ls.builtins.formatting.prettier,
           require("typescript.extensions.null-ls.code-actions"),
         },
