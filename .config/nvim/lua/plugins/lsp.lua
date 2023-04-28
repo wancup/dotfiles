714782f6894e52133490a07ac5e6e22fafac5995
wancup <62416191+wancup@users.noreply.github.com>
2023-04-29 01:15:40 +0900

    Add markdownlint config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -137,6 +137,7 @@ return {
           null_ls.builtins.diagnostics.eslint_d,
           null_ls.builtins.code_actions.eslint_d,
           null_ls.builtins.formatting.prettier,
+          null_ls.builtins.diagnostics.markdownlint,
         },
       }
     end,
