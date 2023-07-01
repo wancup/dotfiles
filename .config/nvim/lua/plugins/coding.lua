c4e26d786cf2b85745fdcfae1cc08cd27024bc0a
wancup <62416191+wancup@users.noreply.github.com>
2023-07-02 08:39:28 +0900

    add dial

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -119,4 +119,33 @@ return {
     },
   },
 
+  -- Better Increment/Decrement
+  {
+    'monaqa/dial.nvim',
+    keys = {
+      { "<C-a>",  "<Plug>(dial-increment)",  mode = { "n", "v" }, desc = "Increment" },
+      { "<C-x>",  "<Plug>(dial-decrement)",  mode = { "n", "v" }, desc = "Decrement" },
+      { "g<C-a>", "g<Plug>(dial-increment)", mode = { "n", "v" }, desc = "Increment" },
+      { "g<C-x>", "g<Plug>(dial-decrement)", mode = { "n", "v" }, desc = "Decrement" },
+    },
+    config = function()
+      local augend = require("dial.augend")
+      require("dial.config").augends:register_group {
+        default = {
+          augend.integer.alias.decimal,
+          augend.date.alias["%Y/%m/%d"],
+          augend.date.alias["%Y-%m-%d"],
+          augend.semver.alias.semver,
+          augend.constant.alias.bool,
+          augend.constant.new {
+            elements = { "&&", "||" },
+            word = true,
+            cyclic = true,
+          },
+          augend.misc.alias.markdown_header,
+        },
+      }
+    end,
+  },
+
 }
