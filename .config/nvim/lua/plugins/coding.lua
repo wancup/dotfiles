29afafb939c55f1ae0bb57c4b886b3609a402020
wancup <62416191+wancup@users.noreply.github.com>
2023-07-01 11:42:02 +0900

    Update jump settings

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -85,7 +85,20 @@ return {
   {
     "ggandor/leap.nvim",
     config = function()
-      require('leap').add_default_mappings()
+      local leap = require('leap')
+      leap.add_default_mappings()
+      leap.opts.highlight_unlabeled_phase_one_targets = true
+      vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Comment' })
+    end
+  },
+  {
+    "jinh0/eyeliner.nvim",
+    config = function()
+      require 'eyeliner'.setup {
+        highlight_on_key = true,
+        dim = true,
+      }
+      vim.api.nvim_set_hl(0, "EyelinerDimmed", { link = "Comment" })
     end
   },
 
