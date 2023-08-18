20bee2cc0fac445d0e71f573eb808d0d30d260bd
wancup <62416191+wancup@users.noreply.github.com>
2023-08-18 15:19:00 +0900

    Change the rename plugin

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -25,15 +25,26 @@ return {
 
   -- Better Rename
   {
-    "smjonas/inc-rename.nvim",
+    "filipdutescu/renamer.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
-      { "<leader>rn", "<cmd>IncRename <cr>", desc = "[R]e[N]ame" },
+      { "<leader>rn", "<cmd>lua require('renamer').rename()<cr>", desc = "[R]e[N]ame" },
     },
     config = function()
-      require("inc_rename").setup()
-      vim.keymap.set("n", "<leader>rn", function()
-        return ":IncRename " .. vim.fn.expand("<cword>")
-      end, { expr = true, desc = "[R]e[N]ame" })
+      local mappings_utils = require("renamer.mappings.utils")
+      require("renamer").setup({
+        mappings = {
+          ["<c-i>"] = nil,
+          ["<c-a>"] = mappings_utils.set_cursor_to_start,
+          ["<c-e>"] = mappings_utils.set_cursor_to_end,
+          ["<c-b>"] = nil,
+          ["<c-c>"] = function()
+            vim.api.nvim_input("<esc>")
+          end,
+          ["<c-u>"] = mappings_utils.clear_line,
+          ["<c-r>"] = nil,
+        },
+      })
     end,
   },
 
