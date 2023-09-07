5137852ebfcd11c1fce7efc2946767db8602e047
wancup <62416191+wancup@users.noreply.github.com>
2023-09-07 20:50:37 +0900

    remove unused code_action

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -131,7 +131,6 @@ return {
 			return {
 				on_attach = on_attach,
 				sources = {
-					null_ls.builtins.code_actions.gitsigns,
 					null_ls.builtins.diagnostics.eslint_d,
 					null_ls.builtins.code_actions.eslint_d,
 					null_ls.builtins.formatting.prettierd,
