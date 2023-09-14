7b5b5fdfff799961f097153024a5a1cef69d1d05
wancup <62416191+wancup@users.noreply.github.com>
2023-09-15 02:02:59 +0900

    dont run history command automatically

--- .config/fish/functions/fzf-history.fish
+++ .config/fish/functions/fzf-history.fish
@@ -1,8 +1,6 @@
 function fzf-history
   history -z | eval fzf --scheme=history --read0 --print0 -q '(commandline)' | read -lz result
   if test $status -eq 0
-    echo "RUN: $result"
-    eval $result
-    commandline -f repaint
+    commandline $result
   end
 end
