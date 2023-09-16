15435b821f3cad174ca591749d9749ef6635a589
wancup <62416191+wancup@users.noreply.github.com>
2023-09-16 19:18:17 +0900

    change to use format-on-save

---
+++ .config/nvim/lua/plugins/coding/format-on-save.lua
@@ -0,0 +1,34 @@
+return {
+	"elentok/format-on-save.nvim",
+	event = "BufWritePre",
+	config = function()
+		local formatters = require("format-on-save.formatters")
+		require("format-on-save").setup({
+			exclude_path_patterns = {
+				"/node_modules/",
+			},
+			formatter_by_ft = {
+				lua = formatters.stylua,
+				rust = formatters.lsp,
+				json = formatters.prettierd,
+				markdown = formatters.prettierd,
+				javascript = formatters.prettierd,
+				yaml = formatters.prettierd,
+				typescript = {
+					formatters.if_file_exists({
+						pattern = ".eslintrc.*",
+						formatter = formatters.eslint_d_fix,
+					}),
+					formatters.prettierd,
+				},
+				typescriptreact = {
+					formatters.if_file_exists({
+						pattern = ".eslintrc.*",
+						formatter = formatters.eslint_d_fix,
+					}),
+					formatters.prettierd,
+				},
+			},
+		})
+	end,
+}
