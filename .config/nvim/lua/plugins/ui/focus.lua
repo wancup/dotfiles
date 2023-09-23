d0d603bb73df848194c2af4617d44ca4e1370189
wancup <62416191+wancup@users.noreply.github.com>
2023-09-23 15:17:21 +0900

    Disable auto resize on neo-tree

--- .config/nvim/lua/plugins/ui/focus.lua
+++ .config/nvim/lua/plugins/ui/focus.lua
@@ -13,4 +13,18 @@ return {
 			hybridnumber = true,
 		},
 	},
+	config = function(_, opts)
+		local augroup = vim.api.nvim_create_augroup("FocusDisable", { clear = true })
+		vim.api.nvim_create_autocmd("WinEnter", {
+			group = augroup,
+			callback = function(_)
+				if vim.tbl_contains({ "neo-tree" }, vim.bo.filetype) then
+					require("focus").focus_disable()
+				else
+					require("focus").focus_enable()
+				end
+			end,
+		})
+		require("focus").setup(opts)
+	end,
 }
