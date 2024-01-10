358fda9dd924e30144c9878ed83fefacff4e01e0
wancup <62416191+wancup@users.noreply.github.com>
2024-01-10 12:38:04 +0900

    Change copilot keymap

--- .config/nvim/lua/plugins/coding/copilot.lua
+++ .config/nvim/lua/plugins/coding/copilot.lua
@@ -2,5 +2,13 @@ return {
 	{
 		"github/copilot.vim",
 		cmd = { "Copilot" },
+		init = function()
+			vim.g.copilot_no_tab_map = true
+
+			vim.keymap.set("i", "<C-Cr>", 'copilot#Accept("")', {
+				expr = true,
+				replace_keycodes = false,
+			})
+		end,
 	},
 }
