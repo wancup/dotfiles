e7967ca4c1bec0e23a957f9287546ebdd0dc4d0c
wancup <62416191+wancup@users.noreply.github.com>
2024-01-18 00:16:21 +0900

    Add missing XDG env bars

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -38,7 +38,9 @@ set -g fish_vi_force_cursor 1
 
 # environments
 set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx XDG_CACHE_HOME $HOME/.cache
 set -gx XDG_DATA_HOME $HOME/.local/share
+set -gx XDG_STATE_HOME $HOME/.local/state
 set -gx AQUA_ROOT_DIR $XDG_DATA_HOME/aquaproj-aqua
 set -gx AQUA_GLOBAL_CONFIG $XDG_CONFIG_HOME/aquaproj-aqua/aqua.yaml
 set -gx MANPAGER 'nvim +Man!'
