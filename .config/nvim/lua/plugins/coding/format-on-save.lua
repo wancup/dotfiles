5b3525d1a51d69e0ede3795ba47f30c8ecadef93
wancup <62416191+wancup@users.noreply.github.com>
2024-01-25 01:50:18 +0900

    change formatter settings

--- .config/nvim/lua/plugins/coding/format-on-save.lua
+++ .config/nvim/lua/plugins/coding/format-on-save.lua
@@ -1,3 +1,6 @@
+local eslint_config = ".eslintrc.*"
+local prettier_config = { ".prettierrc", ".prettierrc.*", "prettier.config.*" }
+
 return {
 	"elentok/format-on-save.nvim",
 	event = "BufWritePre",
@@ -9,25 +12,70 @@ return {
 				"/node_modules/",
 			},
 			formatter_by_ft = {
-				lua = formatters.stylua,
 				rust = formatters.lsp,
-				json = formatters.prettierd,
-				markdown = formatters.prettierd,
-				javascript = formatters.prettierd,
-				yaml = formatters.prettierd,
-				typescript = {
+				lua = {
 					formatters.if_file_exists({
-						pattern = ".eslintrc.*",
+						pattern = { "stylua.toml" },
+						formatter = formatters.stylua,
+					}),
+				},
+				markdown = {
+					formatters.if_file_exists({
+						pattern = prettier_config,
+						formatter = formatters.prettierd,
+					}),
+				},
+				yaml = {
+					formatters.if_file_exists({
+						pattern = prettier_config,
+						formatter = formatters.prettierd,
+					}),
+				},
+				json = {
+					formatters.if_file_exists({
+						pattern = prettier_config,
+						formatter = formatters.prettierd,
+					}),
+				},
+				javascript = {
+					formatters.if_file_exists({
+						pattern = eslint_config,
 						formatter = formatters.eslint_d_fix,
 					}),
-					formatters.prettierd,
+					formatters.if_file_exists({
+						pattern = prettier_config,
+						formatter = formatters.prettierd,
+					}),
+				},
+				javascriptreact = {
+					formatters.if_file_exists({
+						pattern = eslint_config,
+						formatter = formatters.eslint_d_fix,
+					}),
+					formatters.if_file_exists({
+						pattern = prettier_config,
+						formatter = formatters.prettierd,
+					}),
+				},
+				typescript = {
+					formatters.if_file_exists({
+						pattern = eslint_config,
+						formatter = formatters.eslint_d_fix,
+					}),
+					formatters.if_file_exists({
+						pattern = prettier_config,
+						formatter = formatters.prettierd,
+					}),
 				},
 				typescriptreact = {
 					formatters.if_file_exists({
-						pattern = ".eslintrc.*",
+						pattern = eslint_config,
 						formatter = formatters.eslint_d_fix,
 					}),
-					formatters.prettierd,
+					formatters.if_file_exists({
+						pattern = prettier_config,
+						formatter = formatters.prettierd,
+					}),
 				},
 			},
 		})
