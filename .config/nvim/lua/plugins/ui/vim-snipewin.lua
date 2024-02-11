19538dbc7cd502f5703067722846da3ce7d027e1
wancup <62416191+wancup@users.noreply.github.com>
2024-02-11 17:36:21 +0900

    fix snipewin setup

--- .config/nvim/lua/plugins/ui/vim-snipewin.lua
+++ .config/nvim/lua/plugins/ui/vim-snipewin.lua
@@ -3,10 +3,18 @@ return {
 	{
 		"4513ECHO/vim-snipewin",
 		keys = {
-			{ "<C-w>p", "<Cmd>call snipewin#select(g:snipewin#callback#goto)<CR>", desc = "[P]ick Window" },
+			{
+				"<C-w>p",
+				function()
+					require("snipewin").select(vim.fn["g:snipewin#callback#goto"])
+				end,
+				desc = "[P]ick Window",
+			},
 			{
 				"<C-w>c",
-				"<Cmd>call snipewin#select(g:snipewin#callback#close)<CR>",
+				function()
+					require("snipewin").select(vim.fn["g:snipewin#callback#close"])
+				end,
 				desc = "[C]lose Window",
 			},
 		},
