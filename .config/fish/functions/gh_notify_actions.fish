e6dcf990ed539e0da40792df1d05ba1d900716a4
wancup <62416191+wancup@users.noreply.github.com>
2024-02-14 01:15:56 +0900

    add comments to the fish command

--- .config/fish/functions/gh_notify_actions.fish
+++ .config/fish/functions/gh_notify_actions.fish
@@ -1,3 +1,3 @@
-function gh_notify_actions
+function gh_notify_actions -d "Watch gh actions and notify to Desktop when finished"
   gh run watch && osascript -e 'display notification "GitHub Run is DONE!"'
 end
