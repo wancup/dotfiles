6c2828ddc1e5fc6268753ba40bc41fe5d729c460
wancup <62416191+wancup@users.noreply.github.com>
2024-02-14 00:40:32 +0900

    refactor fish functions

--- .config/fish/functions/fzf_git_show_commit.fish
+++ .config/fish/functions/fzf_git_show_commit.fish
@@ -1,5 +1,8 @@
 function fzf_git_show_commit -d "Show a commit using fzf"
-  git log --pretty=oneline --abbrev-commit --reverse | fzf --tac --no-sort --exact --preview "git show --color {1}" | awk "{print \$1;}" | read -l short_hash; \
+  git log --pretty=oneline --abbrev-commit --reverse \
+      | fzf --tac --no-sort --exact --preview "git show --color {1}" \
+      | awk "{print \$1;}" \
+      | read -l short_hash; \
     and git show "$short_hash"; \
     and git rev-parse "$short_hash" | read -l long_hash; \
     and echo "Commit: $long_hash"
--- .config/fish/functions/fzf_git_switch_branch.fish
+++ .config/fish/functions/fzf_git_switch_branch.fish
@@ -1,4 +1,8 @@
 function fzf_git_switch_branch -d "Switch a branch using fzf"
-  git branch --all | grep -v HEAD | string trim | fzf --preview "git log --graph --color {1}" | read -l branch_name; \
+  git branch --all \
+      | grep -v HEAD \
+      | string trim \
+      | fzf --preview "git log --graph --color {1}" \
+      | read -l branch_name; \
     and git switch (echo "$branch_name" | sed "s#remotes/[^/]*/##")
 end
