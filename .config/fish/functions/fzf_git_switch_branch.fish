d8b595e14af78402d6f6d0f8922500415e9f0c87
wancup <62416191+wancup@users.noreply.github.com>
2024-02-14 00:11:25 +0900

    change git alias

--- .config/fish/functions/fzf_git_switch_branch.fish
+++ .config/fish/functions/fzf_git_switch_branch.fish
@@ -1,4 +1,4 @@
-function fzf_git_checkout -d "Checkout a branch using fzf"
+function fzf_git_switch_branch -d "Switch a branch using fzf"
   git branch --all | grep -v HEAD | string trim | fzf --preview "git log --graph --color {1}" | read -l branch_name; \
     and git switch (echo "$branch_name" | sed "s#remotes/[^/]*/##")
 end
