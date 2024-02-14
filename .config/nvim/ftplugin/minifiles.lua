b7497ecaf78f834752586486666a515aa66ed71f
wancup <62416191+wancup@users.noreply.github.com>
2024-02-14 21:42:26 +0900

    add come keymap to mini-files

--- .config/nvim/ftplugin/minifiles.lua
+++ .config/nvim/ftplugin/minifiles.lua
@@ -1,12 +1,41 @@
+local files = require("mini.files")
+
+vim.keymap.set("n", "<leader>e", function()
+	files.close()
+end, { buffer = true, desc = "Close mini.files" })
+
 vim.keymap.set("n", "_", function()
 	local cwd = vim.fn.getcwd()
-	require("mini.files").open(cwd)
-end, { buffer = true })
+	files.open(cwd)
+end, { buffer = true, desc = "Open CDW" })
 
 vim.keymap.set("n", "<cr>", function()
-	require("mini.files").go_in({ close_on_file = true })
+	files.go_in({ close_on_file = true })
 end, { buffer = true })
 
 vim.keymap.set("n", "<S-cr>", function()
-	require("mini.files").go_out()
+	files.go_out()
 end, { buffer = true })
+
+vim.keymap.set("n", "<leader><leader>", function()
+	local entry = files.get_fs_entry()
+	if entry ~= nil then
+		files.close()
+		require("telescope.builtin").live_grep({ search_dirs = { entry.path } })
+	end
+end, { buffer = true, desc = "Live-grep in dir" })
+
+vim.keymap.set("n", "<leader>ff", function()
+	local entry = files.get_fs_entry()
+	if entry ~= nil then
+		files.close()
+		require("telescope.builtin").find_files({ search_dirs = { entry.path } })
+	end
+end, { buffer = true, desc = "[F]ind [F]iles in dir" })
+
+vim.keymap.set("n", "<leader>my", function()
+	local entry = files.get_fs_entry()
+	if entry ~= nil then
+		vim.fn.setreg(vim.v.register, entry.name)
+	end
+end, { buffer = true, desc = "[M]ini.files [Y]ank file name" })
