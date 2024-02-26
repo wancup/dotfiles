b01582d76450cad30840170e3d3bd3b6a893f607
wancup <62416191+wancup@users.noreply.github.com>
2024-02-27 02:16:34 +0900

    refactor install script

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
+set -o errexit -o nounset
 
 base_dir=$(cd "$(dirname "$0")" && pwd)
-ln -s "${base_dir}/.vimrc" "${HOME}/.vimrc"
-ln -s "${base_dir}/.zshrc" "${HOME}/.zshrc"
+top_file_list='.vimrc .zshrc'
+
+for top_file in $top_file_list ; do
+  top_link_path="${HOME}/${top_file}"
+  if [ ! -e "${top_link_path}" ]; then
+    ln -s "${base_dir}/${top_file}" "${top_link_path}"
+  fi
+done
 
 dot_configs="${base_dir}/.config/*"
 for conf in $dot_configs; do
