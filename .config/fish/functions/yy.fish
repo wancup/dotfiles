a72a92765a7d9bef0f05b6bc5a643dd4b141972e
wancup <62416191+wancup@users.noreply.github.com>
2024-03-17 02:46:55 +0900

    setup yazi

---
+++ .config/fish/functions/yy.fish
@@ -0,0 +1,8 @@
+function yy
+	set -l tmp (mktemp -t "yazi-cwd.XXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set -l cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		z "$cwd"
+	end
+	rm -f -- "$tmp"
+end
