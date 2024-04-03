3c787402bd622e3e271cecdf0b222eb667a24346
wancup <62416191+wancup@users.noreply.github.com>
2024-04-04 00:59:02 +0900

    change scrollbar style

--- .config/nvim/lua/plugins/ui/scrollbar.lua
+++ .config/nvim/lua/plugins/ui/scrollbar.lua
@@ -1,4 +1,23 @@
 -- Scrollbar
 return {
-	{ "petertriho/nvim-scrollbar", event = { "BufReadPost", "BufNewFile" }, config = true },
+	{
+		"petertriho/nvim-scrollbar",
+		event = { "BufReadPost", "BufNewFile" },
+		opts = {
+			hide_if_all_visible = true,
+			handle = {
+				blend = 10,
+				color = "#323232",
+			},
+			marks = {
+				Cursor = {
+					color = "#666666",
+				},
+			},
+			handlers = {
+				cursor = false,
+				gitsigns = true,
+			},
+		},
+	},
 }
