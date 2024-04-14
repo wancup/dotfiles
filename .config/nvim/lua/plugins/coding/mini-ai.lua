8b97199dc0917e11c00a81285598c2570cb0d7e4
wancup <62416191+wancup@users.noreply.github.com>
2024-04-15 00:05:19 +0900

    add regex textobj

--- .config/nvim/lua/plugins/coding/mini-ai.lua
+++ .config/nvim/lua/plugins/coding/mini-ai.lua
@@ -4,10 +4,12 @@ return {
 		version = false,
 		event = { "BufReadPost", "BufNewFile" },
 		config = function()
+			local gen_spec = require("mini.ai").gen_spec
 			require("mini.ai").setup({
 				search_method = "cover",
 				custom_textobjects = {
 					[" "] = { "[,.({].-[,.)};]", "^().().*().()$" },
+					["r"] = gen_spec.pair("/", "/"),
 				},
 			})
 		end,
