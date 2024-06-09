5608cd24133908fe9e521a355fda64115ae4ffa0
wancup <62416191+wancup@users.noreply.github.com>
2024-06-09 16:06:36 +0900

    remove unused file

--- .config/nvim/lua/plugins/init.lua
+++
@@ -1,8 +0,0 @@
-require("plugins.cmp")
-require("plugins.coding")
-require("plugins.color")
-require("plugins.git")
-require("plugins.lsp")
-require("plugins.syntax")
-require("plugins.telescope")
-require("plugins.ui")
