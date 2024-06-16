5acc12def1acf3a795d93b8d8f373ee37cb2f4f0
wancup <62416191+wancup@users.noreply.github.com>
2024-06-16 21:43:05 +0900

    setup copilot-chat

---
+++ .config/nvim/lua/plugins/ai/copilot-chat.lua
@@ -0,0 +1,50 @@
+return {
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "canary",
+		dependencies = {
+			"github/copilot.vim",
+			"nvim-lua/plenary.nvim",
+		},
+		cmd = {
+			"CopilotChat",
+			"CopilotChatOpen",
+		},
+		opts = {
+			prompts = {
+				Explain = {
+					prompt = "/COPILOT_EXPLAIN 選択しているコードの説明を段落分けした文章で説明してください。",
+				},
+				Review = {
+					prompt = "/COPILOT_REVIEW 選択したコードをレビューしてください。",
+				},
+				Fix = {
+					prompt = "/COPILOT_GENERATE このコードには問題があります。バグを修正したコードを書いてください。",
+				},
+				Optimize = {
+					prompt = "/COPILOT_GENERATE 選択したコードのパフォーマンスと可読性を改善してください。",
+				},
+				Docs = {
+					prompt = "/COPILOT_GENERATE このセクションにドキュメントコメントを追加してください。",
+				},
+				Tests = {
+					prompt = "/COPILOT_GENERATE このコードのテストを生成してください。",
+				},
+				FixDiagnostic = {
+					prompt = "このファイルについての以下のdiagnosticsの問題を解説し、修正したコード案を提示してください。",
+					selection = select.diagnostics,
+				},
+				Commit = {
+					prompt = "コミットメッセージを書いてください。メッセージはすべてをgitcommit形式のコードブロックとしてラップしてください。",
+					selection = select.gitdiff,
+				},
+				CommitStaged = {
+					prompt = "コミットメッセージを書いてください。メッセージはすべてをgitcommit形式のコードブロックとしてラップしてください。",
+					selection = function(source)
+						return select.gitdiff(source, true)
+					end,
+				},
+			},
+		},
+	},
+}
