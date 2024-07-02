f3e1bde99855d396b1974b6e59bc825f8bf052f1
wancup <62416191+wancup@users.noreply.github.com>
2024-07-03 01:14:27 +0900

    disable copilot autoenable

--- .config/nvim/lua/plugins/ai/copilot.lua
+++ .config/nvim/lua/plugins/ai/copilot.lua
@@ -3,6 +3,7 @@ return {
 		"github/copilot.vim",
 		cmd = { "Copilot" },
 		init = function()
+			vim.g.copilot_enabled = false
 			vim.g.copilot_no_tab_map = true
 
 			vim.keymap.set("i", "<C-Cr>", 'copilot#Accept("")', {
