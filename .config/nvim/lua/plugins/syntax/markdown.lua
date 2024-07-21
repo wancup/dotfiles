241a61120bf1e64e344d852ea6312e88827f16a1
wancup <62416191+wancup@users.noreply.github.com>
2024-07-21 18:33:58 +0900

    update render-markdown setting

--- .config/nvim/lua/plugins/syntax/markdown.lua
+++ .config/nvim/lua/plugins/syntax/markdown.lua
@@ -1,14 +1,12 @@
 return {
 	{
 		"MeanderingProgrammer/markdown.nvim",
-		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
 		name = "render-markdown",
 		ft = "markdown",
-		keys = { { "<leader>um", "<cmd>RenderMarkdownToggle<cr>", desc = "[U]i [M]arkdown Render Toggle" } },
+		keys = { { "<leader>um", "<cmd>RenderMarkdown toggle<cr>", desc = "[U]i [M]arkdown Render Toggle" } },
 		opts = {
-			start_enabled = false,
-			latex_enabled = false,
-			max_file_size = 1,
+			enabled = false,
 		},
 	},
 }
