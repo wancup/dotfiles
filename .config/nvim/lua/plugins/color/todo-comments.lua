74636d62a7c39cfa8b5a9b5ca59e0959518f26b1
wancup <62416191+wancup@users.noreply.github.com>
2024-08-17 00:25:00 +0900

    add goto todo keymap

--- .config/nvim/lua/plugins/color/todo-comments.lua
+++ .config/nvim/lua/plugins/color/todo-comments.lua
@@ -8,6 +8,20 @@ return {
 		keys = {
 			{ "<leader>lt", "<cmd>TodoQuickFix<cr>", desc = "[L]ist [T]odo" },
 			{ "<leader>ft", "<cmd>TodoTelescope<cr>", desc = "[F]ind [T]odos" },
+			{
+				"[t",
+				function()
+					require("todo-comments").jump_next()
+				end,
+				desc = "Goto previous TODO",
+			},
+			{
+				"]t",
+				function()
+					require("todo-comments").jump_next()
+				end,
+				desc = "Goto next TODO",
+			},
 		},
 	},
 }
