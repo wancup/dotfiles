6f5b878e8c2559437eb46bcf8254f9a936f0e2a3
wancup <62416191+wancup@users.noreply.github.com>
2024-09-03 00:55:08 +0900

    rename render-markdown plugin

--- .config/nvim/lua/plugins/syntax/render-markdown.lua
+++ .config/nvim/lua/plugins/syntax/render-markdown.lua
@@ -1,8 +1,7 @@
 return {
 	{
-		"MeanderingProgrammer/markdown.nvim",
+		"MeanderingProgrammer/render-markdown.nvim",
 		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-		name = "render-markdown",
 		ft = "markdown",
 		keys = { { "<leader>um", "<cmd>RenderMarkdown toggle<cr>", desc = "[U]i [M]arkdown Render Toggle" } },
 		opts = {
