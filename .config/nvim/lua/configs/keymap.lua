69ffd061870425c1ebd13eeb06a82bbff26e6628
wancup <hebium@wancup.dev>
2024-10-05 15:12:08 +0900

    change nvim keymap

--- .config/nvim/lua/configs/keymap.lua
+++ .config/nvim/lua/configs/keymap.lua
@@ -118,7 +118,7 @@ map("n", "<leader>ll", "<cmd>copen<cr>", { desc = "[L]ist Quickfix" })
 map("n", "<leader>lc", git.conflict_list, { desc = "[L]ist Git [C]onflict" })
 map("n", "<leader>ld", diagnostic.current_buf_list, { desc = "[L]ist [D]iagnostics" })
 map("n", "<leader>lD", diagnostic.workspace_list, { desc = "[L]ist [D]iagnostics(Workspace)" })
-map("n", "<C-z>", register.edit_register, { desc = "Edit Register" })
+map("n", "<C-e>", register.edit_register, { desc = "Edit Register" })
 
 -- Insert
 map("i", "<C-h>", "<BS>")
