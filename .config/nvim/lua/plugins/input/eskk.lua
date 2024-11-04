398dfdfb97cc24d16d5ea681174b57140f8f9c9a
wancup <hebium@wancup.dev>
2024-11-04 15:12:54 +0900

    setup eskk

---
+++ .config/nvim/lua/plugins/input/eskk.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"vim-skk/eskk.vim",
+		cond = false,
+		event = { "InsertEnter", "CmdlineEnter" },
+		init = function()
+			vim.g["eskk#directory"] = "~/.local/state/eskk"
+			vim.g["eskk#dictionary"] = "~/.local/state/eskk/SKK-JISYO.user"
+			vim.g["eskk#large_dictionary"] = "~/.local/share/skk/SKK-JISYO.L"
+			vim.g["eskk#show_candidates_count"] = 20
+			vim.g["eskk#show_annotation"] = 1
+			vim.g["eskk#enable_completion"] = 0
+			vim.g["eskk#egg_like_newline"] = 1
+		end,
+	},
+}
