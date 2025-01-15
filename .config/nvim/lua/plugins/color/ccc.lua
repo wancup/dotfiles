930aa52a0218b29f9b2d053b9d7444ab6ba957e9
wancup <hebium@wancup.dev>
2025-01-15 21:18:55 +0900

    add keymap

--- .config/nvim/lua/plugins/color/ccc.lua
+++ .config/nvim/lua/plugins/color/ccc.lua
@@ -5,6 +5,7 @@ return {
 	cmd = { "CccPick", "CccConvert", "CccHighlighterEnable" },
 	keys = {
 		{ "<leader>uc", "<cmd>CccHighlighterToggle<cr>", desc = "[U]i [C]olor Highlight Toggle" },
+		{ "<leader>cc", "<cmd>CccPick<cr>", desc = "[C]omand [C]olor" },
 	},
 	opts = {
 		highlighter = {
