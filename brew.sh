83c187b00d6732dc4d96beac75a33b517f4a67e5
wancup <hebium@wancup.dev>
2025-02-21 22:38:07 +0900

    add setup for macskk

--- brew.sh
+++ brew.sh
@@ -10,6 +10,7 @@ brew install gitmoji
 
 if [ "$(uname)" = 'Darwin' ]; then
   brew install wezterm
+  brew install --cask mtgto/macskk/macskk
   brew install --cask nikitabobko/tap/aerospace
 
   brew install font-noto-emoji
