014443bde258870b87b4ad4ba541589e1c659bbb
wancup <hebium@wancup.dev>
2025-03-20 17:15:20 +0900

    show file path in incline

--- .config/nvim/lua/plugins/ui/incline.lua
+++ .config/nvim/lua/plugins/ui/incline.lua
@@ -1,3 +1,5 @@
+local MAX_FILE_PATH_LENGTH = 15
+
 return {
 	"b0o/incline.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -12,11 +14,15 @@ return {
 		},
 		render = function(props)
 			local buf_name = vim.api.nvim_buf_get_name(props.buf)
+			local file_path = vim.fn.fnamemodify(buf_name, ":p:.:h")
 			local file_name = vim.fn.fnamemodify(buf_name, ":t")
 			local file_extension = vim.fn.fnamemodify(file_name, ":e")
 			local icon, icon_color =
 				require("nvim-web-devicons").get_icon_color(file_name, file_extension, { default = true })
 			local modified = vim.bo[props.buf].modified
+			local shorten_file_path = string.len(file_path) > MAX_FILE_PATH_LENGTH
+					and ".." .. string.sub(file_path, -MAX_FILE_PATH_LENGTH)
+				or file_path
 			return {
 				{
 					" ",
@@ -25,7 +31,9 @@ return {
 					guifg = icon_color,
 				},
 				" ",
-				{ file_name, gui = modified and "bold,italic" or nil },
+				{ shorten_file_path },
+				"/",
+				{ file_name, gui = modified and "bold,italic" or "bold" },
 				" ",
 			}
 		end,
