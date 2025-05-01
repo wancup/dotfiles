472de287fa7bacdbc5581a4e9f87ee2af7faa2a0
wancup <hebium@wancup.dev>
2025-05-01 18:53:16 +0900

    update delete_buffer command

--- .config/nvim/lua/core/buffer.lua
+++ .config/nvim/lua/core/buffer.lua
@@ -1,11 +1,25 @@
 local M = {}
 
+local function is_buf_already_showed(bufnr)
+	local windows = vim.api.nvim_list_wins()
+	for _, win in ipairs(windows) do
+		if vim.api.nvim_win_get_buf(win) == bufnr then
+			return true
+		end
+	end
+	return false
+end
+
 M.delete_buffer = function()
 	local current_buf = vim.api.nvim_get_current_buf()
 	local alt_buf = vim.fn.bufnr("#")
 	if vim.api.nvim_buf_is_valid(alt_buf) then
-		vim.api.nvim_set_current_buf(alt_buf)
-		vim.api.nvim_buf_delete(current_buf, {})
+		if is_buf_already_showed(alt_buf) then
+			vim.cmd("bdelete")
+		else
+			vim.api.nvim_set_current_buf(alt_buf)
+			vim.api.nvim_buf_delete(current_buf, {})
+		end
 	else
 		vim.notify("Alternate buffer is invalid!")
 	end
