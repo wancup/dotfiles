fddc4288a356a9c1eeaf5d186b9123667525aa27
wancup <hebium@wancup.dev>
2025-05-19 20:35:42 +0900

    disable snippet on manually completion

--- .config/nvim/lua/plugins/input/nvim-cmp.lua
+++ .config/nvim/lua/plugins/input/nvim-cmp.lua
@@ -34,7 +34,15 @@ return {
 						},
 					})
 				else
-					cmp.complete()
+					cmp.complete({
+						config = {
+							sources = {
+								{ name = "nvim_lsp", group_index = 1 },
+								{ name = "nvim_lsp_signature_help", group_index = 1 },
+								{ name = "copilot", group_index = 2 },
+							},
+						},
+					})
 				end
 			end,
 			["<CR>"] = cmp.mapping(cmp.mapping.confirm({ select = false }), { "i", "s", "c" }),
