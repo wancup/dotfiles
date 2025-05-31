a66646df616d1857b3a1acff613d9f202b2dc43a
wancup <hebium@wancup.dev>
2025-05-31 20:21:46 +0900

    fix a conflict in the neotest adopter configurations

--- .config/nvim/lua/plugins/command/neotest.lua
+++ .config/nvim/lua/plugins/command/neotest.lua
@@ -1,3 +1,24 @@
+local test_ext_regexes = {
+	"%.spec%.js$",
+	"%.spec%.jsx$",
+	"%.test%.js$",
+	"%.test%.jsx$",
+	"%.spec%.ts$",
+	"%.spec%.tsx$",
+	"%.test%.ts$",
+	"%.test%.tsx$",
+}
+
+local function is_e2e_test_dir(file_path)
+	local e2e_dirs = { "tests", "e2e" }
+	for _, dir in ipairs(e2e_dirs) do
+		if string.match(file_path, dir) then
+			return true
+		end
+	end
+	return false
+end
+
 return {
 	"nvim-neotest/neotest",
 	dependencies = {
@@ -74,8 +95,40 @@ return {
 	config = function()
 		require("neotest").setup({
 			adapters = {
-				require("neotest-vitest"),
-				require("neotest-playwright").adapter({ options = {} }),
+				require("neotest-vitest")({
+					is_test_file = function(file_path)
+						if is_e2e_test_dir(file_path) then
+							return false
+						end
+
+						if string.match(file_path, "__tests__") then
+							return true
+						end
+
+						for _, ext in ipairs(test_ext_regexes) do
+							if string.match(file_path, ext) then
+								return true
+							end
+						end
+						return false
+					end,
+				}),
+				require("neotest-playwright").adapter({
+					options = {
+						is_test_file = function(file_path)
+							if not is_e2e_test_dir(file_path) then
+								return false
+							end
+
+							for _, ext in ipairs(test_ext_regexes) do
+								if string.match(file_path, ext) then
+									return true
+								end
+							end
+							return false
+						end,
+					},
+				}),
 			},
 			consumers = {
 				playwright = require("neotest-playwright.consumers").consumers,
