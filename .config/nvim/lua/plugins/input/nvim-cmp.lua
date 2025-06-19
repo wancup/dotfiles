dd9e32341ff67e437f838c8bd0902f704d01a5fb
wancup <contact@wancup.dev>
2025-06-19 23:14:22 +0900

    remove copilot suggestion for default cmp source

--- .config/nvim/lua/plugins/input/nvim-cmp.lua
+++ .config/nvim/lua/plugins/input/nvim-cmp.lua
@@ -63,7 +63,6 @@ return {
 				{ name = "luasnip", group_index = 1 },
 				{ name = "nvim_lsp", group_index = 1 },
 				{ name = "nvim_lsp_signature_help", group_index = 1 },
-				{ name = "copilot", group_index = 2 },
 				per_filetype = {
 					codecompanion = { "codecompanion" },
 				},
