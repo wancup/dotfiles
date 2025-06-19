8256f14e41f294152b7515f4dfd0ba4ece94d671
wancup <contact@wancup.dev>
2025-06-19 22:09:13 +0900

    update cmp setting

--- .config/nvim/lua/plugins/input/nvim-cmp.lua
+++ .config/nvim/lua/plugins/input/nvim-cmp.lua
@@ -33,7 +33,6 @@ return {
 						sources = {
 							{ name = "nvim_lsp", group_index = 1 },
 							{ name = "nvim_lsp_signature_help", group_index = 1 },
-							{ name = "copilot", group_index = 2 },
 						},
 					},
 				})
