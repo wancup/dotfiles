bf03d0d3f11f51912e5028017882c0d4b9327eeb
wancup <contact@wancup.dev>
2025-06-22 15:43:08 +0900

    enable oklch on ccc

--- .config/nvim/lua/plugins/color/ccc.lua
+++ .config/nvim/lua/plugins/color/ccc.lua
@@ -7,9 +7,24 @@ return {
 		{ "<leader>uc", "<cmd>CccHighlighterToggle<cr>", desc = "[U]i [C]olor Highlight Toggle" },
 		{ "<leader>cc", "<cmd>CccPick<cr>", desc = "[C]omand [C]olor" },
 	},
-	opts = {
-		highlighter = {
-			auto_enable = true,
-		},
-	},
+	config = function()
+		require("ccc").setup({
+			highlighter = {
+				auto_enable = true,
+			},
+			inputs = {
+				require("ccc.input.oklch"),
+				require("ccc.input.hsl"),
+				require("ccc.input.rgb"),
+				require("ccc.input.cmyk"),
+			},
+			outputs = {
+				require("ccc.output.css_oklch"),
+				require("ccc.output.css_hsl"),
+				require("ccc.output.css_rgb"),
+				require("ccc.output.hex"),
+				require("ccc.output.hex_short"),
+			},
+		})
+	end,
 }
