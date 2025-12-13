b90f12fe6be6ce56838b4ba07a6a95b6d59f2417
wancup <contact@wancup.dev>
2025-12-13 22:32:01 +0900

    enable gopls inlay hints

---
+++ .config/nvim/after/lsp/gopls.lua
@@ -0,0 +1,14 @@
+return {
+	settings = {
+		gopls = {
+			hints = {
+				assignVariableTypes = true,
+				compositeLiteralFields = true,
+				functionTypeParameters = true,
+				ignoredError = true,
+				parameterNames = true,
+				rangeVariableTypes = true,
+			},
+		},
+	},
+}
