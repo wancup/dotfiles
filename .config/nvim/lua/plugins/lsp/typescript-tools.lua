a26dffffa734a2a30d9c9579e339b7d4f0b0d607
wancup <contact@wancup.dev>
2025-12-13 22:16:47 +0900

    enable ts inlayhints

--- .config/nvim/lua/plugins/lsp/typescript-tools.lua
+++ .config/nvim/lua/plugins/lsp/typescript-tools.lua
@@ -7,6 +7,16 @@ return {
 			separate_diagnostic_server = true,
 			publish_diagnostic_on = "insert_leave",
 			expose_as_code_action = "all",
+			tsserver_file_preferences = {
+				includeInlayParameterNameHints = "all",
+				includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+				includeInlayFunctionParameterTypeHints = true,
+				includeInlayVariableTypeHints = true,
+				includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+				includeInlayPropertyDeclarationTypeHints = true,
+				includeInlayFunctionLikeReturnTypeHints = true,
+				includeInlayEnumMemberValueHints = true,
+			},
 		},
 	},
 }
