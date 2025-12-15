338980b163f169eeecbb78bf642fff42b78b97cd
wancup <contact@wancup.dev>
2025-12-15 14:04:56 +0900

    remove unused tsserver command

--- .config/nvim/lua/plugins/lsp/typescript-tools.lua
+++ .config/nvim/lua/plugins/lsp/typescript-tools.lua
@@ -6,7 +6,12 @@ return {
 		settings = {
 			separate_diagnostic_server = true,
 			publish_diagnostic_on = "insert_leave",
-			expose_as_code_action = "all",
+			expose_as_code_action = {
+				"add_missing_imports",
+				"organize_imports",
+				"remove_unused",
+				"remove_unused_imports",
+			},
 			tsserver_file_preferences = {
 				includeInlayParameterNameHints = "all",
 				includeInlayParameterNameHintsWhenArgumentMatchesName = true,
